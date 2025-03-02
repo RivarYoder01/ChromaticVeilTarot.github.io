7fb040193f5044d9014aba2187d1c1c0c1797229
RivarYoder01 <alyssa.stars.101@gmail.com>
2025-03-02 12:13:03 -0600

    screaming

--- cardImages.js
+++ cardImages.js
@@ -1,1 +1,21 @@
+"use strict";
+/*    JavaScript 7th Edition
+      Chapter 5
+      Chapter Case
+
+      Image List
+
+      Filename:lightbox_data.js
+*/
+
+// Title of the slideshow
+let lightboxTitle = "My Western Vacation";
+
+// Names of the image files shown in the slideshow
+let imgFiles = ["0Fool.png", "1Magician.png", "2HighPriestess.png", "3Empress.png",
+                "4Emperor.png", "5Hierophant.png"]
+
+
+// Count of images in the slideshow
+let imgCount = imgFiles.length;
 
