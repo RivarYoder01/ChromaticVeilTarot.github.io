8d30fb0111c70169408279b40a95b301d85dec2e
RivarYoder01 <alyssa.stars.101@gmail.com>
2025-03-02 12:18:33 -0600

    removing elements

--- interaction.js
+++ interaction.js
@@ -17,50 +17,7 @@ function createLightbox(){
 	let lightBox = document.getElementById("lightbox");
 	
 	// Parts of the lightBox
-	let lbTitle = document.createElement("h1");
-	let lbCounter = document.createElement("div");
-	let lbPrev = document.createElement("div");
-	let lbNext = document.createElement("div");
-	let lbPlay = document.createElement("div");
 	let lbImages = document.createElement("div");
-	
-	// Design the lightbox title
-	lightBox.appendChild(lbTitle);
-	lbTitle.id = "lbTitle";
-	lbTitle.textContent = lightboxTitle;
-	
-	// Design the lightbox slide counter
-	lightBox.appendChild(lbCounter);
-	lbCounter.id = "lbCounter";
-	let currentImg = 1;
-	lbCounter.textContent = currentImg + " / " + imgCount;
-	
-	// Design the lightbox previous slide button
-	lightBox.appendChild(lbPrev);
-	lbPrev.id = "lbPrev";
-	lbPrev.innerHTML = "&#9664;";
-	lbPrev.onclick = showPrev;
-	
-	// Design the lightbox next slide button
-	lightBox.appendChild(lbNext);
-	lbNext.id = "lbNext";
-	lbNext.innerHTML = "&#9654;";
-	lbNext.onclick = showNext;
-	
-	// Design the lightbox Play-Pause button
-	lightBox.appendChild(lbPlay);
-	lbPlay.id = "lbPlay";
-	lbPlay.innerHTML = "&#9199;";
-	let timeID;
-	lbPlay.onclick = function() {
-		if (timeID) { // Stop the slideshow
-			window.clearInterval(timeID);
-			timeID = undefined;
-		} else { // Start the slideshow
-			showNext();
-			timeID = window.setInterval(showNext, 1500);
-		}
-	}
 	
 	// Design the lightbox images container
 	lightBox.appendChild(lbImages);
